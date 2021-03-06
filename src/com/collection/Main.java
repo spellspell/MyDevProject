84c2438aac51615f632c548a5e50d2f6fd008d7b
spellspell <spelspel12@gmail.com>
2021-03-06 12:12:59 -0500

    This is second commit

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         for (int i = 0; i <5 ; i++) {
             System.out.println("Hello batch 8");
-            System.out.println("oh yeahh");
+            System.out.println("oh yeah");
             Thread.sleep(2000);
         }
     }
