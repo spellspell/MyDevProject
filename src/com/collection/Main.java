e66798a18348c4a0cc172914cb473c4b253a2c61
spellspell <spelspel12@gmail.com>
2021-03-06 12:23:22 -0500

    loop change

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -2,7 +2,7 @@ package com.collection;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i <5 ; i++) {
+        for (int i = 0; i <6 ; i++) {
             System.out.println("Hello batch 8");
             System.out.println("oh yeah");
             Thread.sleep(2000);
